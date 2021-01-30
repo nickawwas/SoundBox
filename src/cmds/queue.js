1edf58d227f750309d9882793780fdd6bd73ffa8
Nicholas Kawwas <nicholas.b.kawwas@gmail.com>
2021-01-29 22:25:27 -0800

    Updated Queue

--- src/cmds/queue.js
+++ src/cmds/queue.js
@@ -1,5 +1,7 @@
 import search from './search.js';
 
+import { MessageEmbed } from 'discord.js';
+
 //Add Music to Queue
 const queue = async (msg, args, musicQ) => {
     const [found, content] = await search(args);
@@ -8,7 +10,15 @@ const queue = async (msg, args, musicQ) => {
         return msg.channel.send(content);
 
     musicQ.push(content);
-    msg.channel.send(`Enqueued:  **${content.title}** !\n${content.url}`);
+
+    //Embedded Message
+    const newEmbedded = new MessageEmbed()
+        .setColor('#1fa19c')
+        .setTitle('Enqueued: ')
+        .setDescription(`[${content.title}](${content.url})`)
+        .setFooter('\nEnjoy SoundBox!');
+
+    msg.channel.send(newEmbedded);  
 }
 
 export default queue;
